40551cca6b4e420fd6a330cce749bae9d0d181a6
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-01-29 18:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	let result=s.split(' ');
+	for(i=0;i<1;i++){
+		return result[i];
+	}
+	
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
